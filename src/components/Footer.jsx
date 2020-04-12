b3fdfa9203068d4fb2d6b88e879e6f9db0bb2881
Darshanmesta <46439087+Darshanmesta@users.noreply.github.com>
2020-04-12 20:02:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,18 @@ function Footer (){
     
     <div>
         <div className="foot1">
-            <p><em>copyright limited ©</em></p>
+              <p><em>Please Follow Us on </em></p>
+              <div className="icons">
+            <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+            <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter-square fa-2x"></i></a>
+            <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+              </div>
+              <div className="copyRight"> <p><em>Copyrights limited ©</em></p></div>
+
+              <div className="end">
+                  <p> <em>Thank You For Visiting Us</em></p>
+              </div>
+           
         </div>
     </div>
     
